e4737355af8ba351fa37d7b118b473f1386454d4
maojude <maojude04@gmail.com>
2025-06-03 14:24:06 +0800

    Finish the Etch-A-Sketch Project

--- backend.js
+++ backend.js
@@ -1,24 +1,51 @@
 const container = document.querySelector(".container");
 
+function createGrid(numRows) {
+  const TOTAL_WIDTH = 500;
+  let cellSize = TOTAL_WIDTH / numRows;
 
+  for (let i = 0; i < numRows * numRows; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
 
-function createGrid (numRows){
+    cell.style.width = cellSize + "px";
 
-    const CELLSIZE = 20;
-    let totalWidth = CELLSIZE * numRows;
+    container.appendChild(cell);
 
-    container.style.width = totalWidth + 'px';
-
-
-
-    for(let i = 0; i < (16*16); i++){
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-    
-        container.appendChild(cell);
-    }
+    cell.addEventListener("pointerenter", function (e) {
+      e.target.style.backgroundColor = "gray";
+    });
+  }
 }
 
-createGrid(16, 16);
+createGrid(16);
 
+const button = document.querySelector("button");
+button.addEventListener("click", function (e) {
+  let numRows = promptInput();
 
+  deleteGrid();
+  createGrid(numRows);
+});
+
+function deleteGrid() {
+  container.innerHTML = "";
+}
+
+function promptInput() {
+  let input = prompt("Please enter number of squares per side (1–100):");
+  let numRows = Number(input);
+
+  if (isNaN(numRows)) {
+    alert("Please enter a valid number.");
+    return promptInput(); // retry
+  } else if (numRows <= 0) {
+    alert("Error: Enter a number greater than 0.");
+    return promptInput(); // retry
+  } else if (numRows > 100) {
+    alert("Error: Enter a number less than or equal to 100.");
+    return promptInput(); // retry
+  } else {
+    return numRows;
+  }
+}
